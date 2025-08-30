7f8ca7635c74dbbd0b10c0c5f5ece52a27eac829
Christian Lugo <christianlugo1503@gmail.com>
2025-08-30 16:02:49 -0700

    Reconfiguracion del script de instalación, pequeño cambio en waybar

--- install.sh
+++ install.sh
@@ -2,155 +2,223 @@ set -euo pipefail
 
 #Variables
 DOTFILES_DIR=$(pwd)
+HAS_YAY=0
 
-#Instalar programas necesarios
-sudo pacman -S --needed --noconfirm cava
-sudo pacman -S --needed --noconfirm rofi-wayland
-sudo pacman -S --needed --noconfirm hyprpicker
-sudo pacman -S --needed --noconfirm swww
-sudo pacman -S --needed --noconfirm otf-codenewroman-nerd
-sudo pacman -S --needed --noconfirm nwg-displays
-sudo pacman -S --needed --noconfirm pavucontrol
-sudo pacman -S --needed --noconfirm brightnessctl
-sudo pacman -S --needed --noconfirm networkmanager
-sudo pacman -S --needed --noconfirm network-manager-applet
-sudo pacman -S --needed --noconfirm nm-connection-editor
-sudo pacman -S --needed --noconfirm dunstwa
-sudo pacman -S --needed --noconfirm waybar
-sudo pacman -S --needed --noconfirm nautilus
-sudo pacman -S --needed --noconfirm zsh
-sudo pacman -S --needed --noconfirm hyprshot
-sudo pacman -S --needed --noconfirm zoxide
-sudo pacman -S --needed --noconfirm wl-clipboard
-sudo pacman -S --needed --noconfirm noto-fonts-emoji
-sudo pacman -S --needed --noconfirm xdg-desktop-portal-hyprland
-sudo pacman -S --needed --noconfirm hyprpolkitagent
-sudo pacman -S --needed --noconfirm nwg-look
-yay -S --noconfirm --needed blueman-git  
-yay -S --noconfirm --needed hellwal 
-yay -S --noconfirm --needed waypaper
-yay -S --noconfirm --needed python-pywalfox
-yay -S --noconfirm --needed pokemon-colorscripts-git
-yay -S --noconfirm --needed clipse
-yay -S --noconfirm --needed nmgui-bin
-yay -S --noconfirm --needed sddm-silent-theme
-yay -S --noconfirm --needed bongocat
+#Detectar yay
+if command -v yay >/dev/null 2>&1; then HAS_YAY=1; fi
 
-#               Configuraciónes 
-#Hyprland
-echo "Aplicando configuraciones de Hyprland..."
-rm -rf "$HOME/.config/hypr"
-ln -srv "$DOTFILES_DIR/configs/hypr" "$HOME/.config/hypr"
-echo "Listo"
+#utilidades
+log() { printf '\e[1;34m[INFO] %s\e[0m %s\n' "$*"; }
+warn() { printf '\e[1;33m[WARN] %s\e[0m %s\n' "$*"; }
+err() { printf '\e[1;31m[ERROR] %s\e[0m %s\n' "$*"; }
+ok() { printf '\e[1;32m[OK] %s\e[0m\n' "$*";}
 
-#Bluetooth
-sudo systemctl start bluetooth
-sudo systemctl enable bluetooth
+#Listas
+PACMAN_PACKAGES=(
+    cava rofi-wayland hyprpicker swww otf-codenewroman-nerd nwg-displays
+    pavucontrol brightnessctl network-manager-applet nm-connection-editor
+    dunst waybar nautilus zsh hyprshot zoxide wl-clipboard noto-fonts-emoji
+    xdg-desktop-portal-hyprland hyprpolkitagent nwg-look qt5-quickcontrols2
+    layer-shell-qt5 layer-shell-qt
+)
 
-#Iconos
-echo "Aplicando iconos..."
-rm -rf "$HOME/.local/share/icons"
-ln -srv "$DOTFILES_DIR/configs/icons" "$HOME/.local/share"
+AUR_PACKAGES=(
+    blueman-git hellwal waypaper python-pywalfox pokemon-colorscripts-git 
+    clipse nmgui-bin bongocat
+)
 
-gsettings set org.gnome.desktop.interface icon-theme "kora"
+install_pacman_packages() {
+    log "Sincronizando bases e instalando paquetes de pacman..."
+    sudo pacman -Sy --noconfirm > /dev/null 2>&1
+    sudo pacman -S --needed --noconfirm "${PACMAN_PACKAGES[@]}" > /dev/null 2>&1
+    ok "Paquetes Pacman instalados exitosamente"
+}
 
-echo "Listo"
+install_aur_packages() {
+    if [ "$HAS_YAY" -ne 1 ]; then
+        warn "No encontré 'yay'. Saltando instalación AUR. Si quieres instalar AUR, instala yay o adapta el script."
+    return
+    fi
+        log "Instalando paquetes AUR con yay..."
+        yay -S --noconfirm --needed "${AUR_PACKAGES[@]}" > /dev/null 2>&1
+        ok "Paquetes AUR instalados exitosamente"
 
-#Themes
-echo "Aplicando temas..."
-rm -rf "$HOME/.local/share/themes"
-ln -srv "$DOTFILES_DIR/configs/themes" "$HOME/.local/share"
+}
 
-gsettings set org.gnome.desktop.interface gtk-theme Kripton
-gsettings set org.gnome.desktop.wm.preferences theme Kripton
-echo "Listo"
+configs(){
+    #Hyprland
+    log "Aplicando configuraciones de Hyprland..."
+    rm -rf "$HOME/.config/hypr"
+    ln -srv "$DOTFILES_DIR/configs/hypr" "$HOME/.config/hypr" > /dev/null 2>&1
+    ok "Listo"
 
-#Cursor
-hyprctl setcursor macOS 25
+    #Bluetooth
+    sudo systemctl start bluetooth
+    sudo systemctl enable bluetooth
 
-hyprctl reload
+    #Iconos
+    log "Aplicando iconos..."
+    rm -rf "$HOME/.local/share/icons"
+    ln -srv "$DOTFILES_DIR/configs/icons" "$HOME/.local/share" > /dev/null 2>&1
 
-#Red
-sudo systemctl enable --now NetworkManager
-nm-applet --indicator
+    gsettings set org.gnome.desktop.interface icon-theme "kora"
 
-#Waypaper
-echo "Aplicando configuraciones de Waypaper..."
-rm -rf "$HOME/.config/waypaper"
-ln -srv "$DOTFILES_DIR/configs/waypaper" "$HOME/.config/waypaper"
-echo "Listo"
+    ok "Listo"
 
-#Hellwal
-echo "Aplicando configuraciones de Hellwal..."
+    #Themes
+    log "Aplicando temas..."
+    rm -rf "$HOME/.local/share/themes"
+    ln -srv "$DOTFILES_DIR/configs/themes" "$HOME/.local/share" > /dev/null 2>&1
 
-rm -rf "$HOME/.config/hellwal"
-ln -srv "$DOTFILES_DIR/configs/hellwal" "$HOME/.config/hellwal"
+    gsettings set org.gnome.desktop.interface gtk-theme Kripton
+    gsettings set org.gnome.desktop.wm.preferences theme Kripton
+    ok "Listo"
 
-mkdir -p ~/.cache/hellwal
-mkdir -p ~/.cache/wal
+    #Cursor
+    hyprctl setcursor macOS 25 > /dev/null 2>&1
+    hyprctl reload > /dev/null 2>&1
 
-ln -sf ~/.cache/hellwal/colors ~/.cache/wal/colors
-ln -sf ~/.cache/hellwal/colors.json ~/.cache/wal/colors.json
+    #Red
+    sudo systemctl enable --now NetworkManager
 
-echo "Listo"
+    #Waypaper
+    log "Aplicando configuraciones de Waypaper..."
+    rm -rf "$HOME/.config/waypaper"
+    ln -srv "$DOTFILES_DIR/configs/waypaper" "$HOME/.config/waypaper" > /dev/null 2>&1
+    ok "Listo"
 
-#Waybar
-echo "Aplicando configuraciones de Waybar..."
-rm -rf "$HOME/.config/waybar"
-ln -srv "$DOTFILES_DIR/configs/waybar" "$HOME/.config/waybar"
-echo "Listo"
+    #Hellwal
+    log "Aplicando configuraciones de Hellwal..."
+    rm -rf "$HOME/.config/hellwal"
+    ln -srv "$DOTFILES_DIR/configs/hellwal" "$HOME/.config/hellwal" > /dev/null 2>&1
 
-#sddm
-echo "Aplicando configuraciones de SDDM..."
-rm -f "/etc/sddm.conf"
-sudo ln -srv "$DOTFILES_DIR/configs/sddm.conf" "/etc/sddm.conf"
-echo "Listo"
+    mkdir -p ~/.cache/hellwal
+    mkdir -p ~/.cache/wal
 
-#Rofi
-echo "Aplicando configuraciones de Rofi..."
-rm -rf "$HOME/.config/rofi"
-ln -srv "$DOTFILES_DIR/configs/rofi" "$HOME/.config/rofi"
-echo "Listo"
+    ln -sf ~/.cache/hellwal/colors ~/.cache/wal/colors > /dev/null 2>&1
+    ln -sf ~/.cache/hellwal/colors.json ~/.cache/wal/colors.json > /dev/null 2>&1
 
-#Kitty
-echo "Aplicando configuraciones de Kitty..."
-rm -rf "$HOME/.config/kitty"
-ln -srv "$DOTFILES_DIR/configs/kitty" "$HOME/.config/kitty"
-echo "Listo"
+    ok "Listo"
 
-#Dunst
-echo "Aplicando configuraciones de Dunst..."
-rm -rf "$HOME/.config/dunst"
-ln -srv "$DOTFILES_DIR/configs/dunst" "$HOME/.config/dunst"
-killall dunst
-echo "Listo"
+    #Waybar
+    log "Aplicando configuraciones de Waybar..."
+    rm -rf "$HOME/.config/waybar"
+    ln -srv "$DOTFILES_DIR/configs/waybar" "$HOME/.config/waybar" > /dev/null 2>&1
+    ok "Listo"
 
-# OH MY ZSH 
-set +e
-export RUNZSH=no CHSH=no ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
-rm -rf "$HOME/.oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-set -e
+    #sddm
+    log "Aplicando configuraciones de SDDM..."
+    sudo rm -f "/etc/sddm.conf"
+    sudo ln -srv "$DOTFILES_DIR/configs/sddm.conf" "/etc/sddm.conf" > /dev/null 2>&1
+    
+    #sddm minecraft theme
+    sudo rm -rf ~/sddm-theme-minesddm
+    git clone https://github.com/Davi-S/sddm-theme-minesddm.git ~/sddm-theme-minesddm > /dev/null 2>&1
+    sudo cp -r ~/sddm-theme-minesddm/minesddm /usr/share/sddm/themes/
+    sudo rm -rf ~/sddm-theme-minesddm
+    ok "Listo"
 
-# plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+    #Rofi
+    log "Aplicando configuraciones de Rofi..."
+    rm -rf "$HOME/.config/rofi"
+    ln -srv "$DOTFILES_DIR/configs/rofi" "$HOME/.config/rofi" > /dev/null 2>&1
+    ok "Listo"
 
-rm -f "$HOME/.zshrc"
-ln -sv "$DOTFILES_DIR/configs/.zshrc" "$HOME/.zshrc"
+    #Kitty
+    log "Aplicando configuraciones de Kitty..."
+    rm -rf "$HOME/.config/kitty"
+    ln -srv "$DOTFILES_DIR/configs/kitty" "$HOME/.config/kitty" > /dev/null 2>&1
+    ok "Listo"
 
+    #Dunst
+    log "Aplicando configuraciones de Dunst..."
+    rm -rf "$HOME/.config/dunst"
+    ln -srv "$DOTFILES_DIR/configs/dunst" "$HOME/.config/dunst" > /dev/null 2>&1
+    ok "Listo"
 
-#Fuentes
-echo "Aplicando configuraciones de las Fuentes..."
-rm -rf "$HOME/.local/share/fonts"
-ln -srv "$DOTFILES_DIR/configs/fonts" "$HOME/.local/share/fonts"
-echo "Listo"
+    # OH MY ZSH 
+    log "instalando OMZSH"
+    export RUNZSH=no CHSH=no ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+    export RUNZSH=no CHSH=no ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+    rm -rf "$HOME/.oh-my-zsh"
+    git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git "$HOME/.oh-my-zsh" > /dev/null 2>&1
+    ok "Listo"
 
-#Cambiamos Fondo de Pantalla
-swww-daemon &
-swww img $DOTFILES_DIR/configs/wallpaper.jpg
-hellwal -i $DOTFILES_DIR/configs/wallpaper.jpg --neon-mode --bright-offset 1 && pkill -USR2 waybar & pywalfox update &
-waybar &
+    # plugins
+    log "instalando plugins de OMZSH"
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/plugins/zsh-autosuggestions" > /dev/null 2>&1
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" > /dev/null 2>&1
+    ok "Listo"
 
+    rm -f "$HOME/.zshrc"
+    ln -sv "$DOTFILES_DIR/configs/.zshrc" "$HOME/.zshrc" > /dev/null 2>&1
 
+    #Bongocat
+    sudo usermod -a -G input $USER
+
+    #Fuentes
+    log "Aplicando configuraciones de las Fuentes..."
+    rm -rf "$HOME/.local/share/fonts"
+    ln -srv "$DOTFILES_DIR/configs/fonts" "$HOME/.local/share/fonts"> /dev/null 2>&1
+    ok "Listo"
+
+    log "Configurando aplicaciones..."
+    # Inicia daemon si no está
+    if ! pgrep -x swww-daemon >/dev/null; then
+    swww-daemon &
+    # Espera a que el socket exista
+    while ! swww query >/dev/null 2>&1; do
+        sleep 0.1
+    done
+fi
+
+    # Aplica wallpaper
+    swww img "$DOTFILES_DIR/configs/wallpaper.jpg" > /dev/null 2>&1
+
+    # Hellwal
+    hellwal -i "$DOTFILES_DIR/configs/wallpaper.jpg" --neon-mode --bright-offset 1  > /dev/null 2>&1
+
+    pkill -USR2 waybar > /dev/null 2>&1
+
+    # Actualiza pywalfox
+    pywalfox update & > /dev/null 2>&1
+
+    # Bongocat
+    bongocat -t -c "$DOTFILES_DIR/configs/bongocat/bongocat.conf" > /dev/null 2>&1 &
+
+    ok "Listo"
+}
+
+printTitle(){
+   printf '\e[1;32m%s\e[0m\n' "
+    __________.____                                 
+    \____    /|    |    __ __  ____   ____          
+      /     / |    |   |  |  \/ ___\ /  _ \         
+     /     /_ |    |___|  |  / /_/  >  <_> )        
+    /_______ \|_______ \____/\___  / \____/         
+            \/        \/    /_____/                 
+        .___      __    _____.__.__                 
+      __| _/_____/  |__/ ____\__|  |   ____   ______
+     / __ |/  _ \   __\   __\|  |  | _/ __ \ /  ___/
+    / /_/ (  <_> )  |  |  |  |  |  |_\  ___/ \___ \ 
+    \____ |\____/|__|  |__|  |__|____/\___  >____  >
+         \/                               \/     \/ 
+    "
+}
+
+main() {
+    printTitle
+    install_pacman_packages
+    install_aur_packages
+    configs
+
+    echo "
+    ▄▖   ▌    ▜ ▘  ▗   
+    ▐ ▛▌▛▌▛▌  ▐ ▌▛▘▜▘▛▌
+    ▐ ▙▌▙▌▙▌  ▐▖▌▄▌▐▖▙▌
+                   
+    "
+}
+
+main "$@"
 
