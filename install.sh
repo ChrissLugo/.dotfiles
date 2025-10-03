80a565d2d7e2215dd5f7128f266be50b8fcfa6ff
ChrissLugo <christianlugo1503@gmail.com>
2025-10-02 20:11:15 -0700

    Correcciones varias

--- install.sh
+++ install.sh
@@ -20,6 +20,7 @@ PACMAN_PACKAGES=(
     dunst waybar nautilus zsh hyprshot zoxide wl-clipboard noto-fonts-emoji
     xdg-desktop-portal-hyprland hyprpolkitagent nwg-look qt5-quickcontrols2
     layer-shell-qt5 layer-shell-qt qt5ct qt6ct qt5-wayland qt6-wayland kvantum
+    ntfs-3g exfat-utils dosfstools hfsprogs
 )
 
 AUR_PACKAGES=(
